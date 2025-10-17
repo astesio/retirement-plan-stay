a47b1c9d43f0a450586955f48d13c44432a0e4c6
astesio <astesiojose@gmail.com>
2025-10-16 22:56:30 -0300

    rename file test

--- test/equal.spec.ts
+++
@@ -1,8 +0,0 @@
-import { strict as assert } from 'node:assert/strict';
-import { test } from 'node:test';
-
-void test('some test', async (t) => {
-  await t.test('test', () => {
-    assert.equal(1, 1);
-  });
-});
