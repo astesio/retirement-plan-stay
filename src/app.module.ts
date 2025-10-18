ba491eba9c77f5af7fc7549d970c0009ff4fe1a7
astesio <astesiojose@gmail.com>
2025-10-18 15:57:02 -0300

    remove default module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PersistenceModule } from './infra/persistence.module';
 import { BalanceModule } from './Balance/balance.module';
 
 @Module({
   imports: [PersistenceModule, BalanceModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
