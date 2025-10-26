6996b16925d2404ee6da5da109046219265565fb
astesio <astesiojose@gmail.com>
2025-10-25 23:55:47 -0300

    feat: create a module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PersistenceModule } from './infra/persistence.module';
 import { BalanceModule } from './Balance/balance.module';
 import { RedemptionModule } from './Redemption/redemption.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './User/user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     PersistenceModule,
     BalanceModule,
     RedemptionModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
